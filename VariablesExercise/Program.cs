89ae5fff52341650f5a67dbf1bc0a6250457e48d
Kaleb <Kalebreid15@gmail.com>
2025-06-02 16:57:35 -0400

    initial commit

--- VariablesExercise/Program.cs
+++ VariablesExercise/Program.cs
@@ -5,6 +5,14 @@
         static void Main(string[] args)
         {
             Console.WriteLine("Hello, World!");
+            string myName = "Kaleb";
+            int age = 28;
+            double avg = 89.5;
+            decimal bankAccount = 100000.50m;
+            bool isEmployed = true;
+            char initial = 'K';
+
+            Console.WriteLine($"Name: {myName}\nAge: {age}\nAverage: {avg}\nMoney: {bankAccount}\nInitial: {initial}\nEmployed: {isEmployed}");
         }
     }
 }
